1d21c6e5bba01f70cd7cab37ef85294a6741171a
Dmytro Kotkin <kotkin.lg@gmail.com>
2016-04-21 11:18:21 +0300

    finished js11-12

--- js11-12/js/script.js
+++ js11-12/js/script.js
@@ -9,21 +9,21 @@ $(function(){
     student: 'Студент Восточноукраинского Национального Университета им. В.Даля',
     line1: ' <div class = "line"></div> ',
     want: 'Хочу учить фронтэнд потому что:',
-    reason1: 'заборы строить не интересно',
-    reason2: 'платят мало',
-    reason3: 'приходиться работать по ночам',
+    reason1: 'это безумно круто!',
+    reason2: 'это приносит удовольствие!',
+    reason3: 'это заставляет шевелить мозгами :)',
     line2: " <div class = 'line'></div> ",
     contact: 'Мой контактный номер телефона:',
     phone: '+38 050 068 9212',
     profile: 'Мой профиль Вконтакте:',
-    vk: '<a href = "http://vk.com/dkotkin">Дмитрий Коткин</a>',
+    vk: '<a href = "http://vk.com/dkotkin" target="_blank">Дмитрий Коткин</a>',
     line3: " <div class = 'line'></div> ",
     feedback: 'Мой фидбэк:',
-    feedText: 'Могу построить вам забор'
+    feedText: 'Вот-вот и сверстаю первый свой проект. Следите за обновлениями на сайте!'
   };
 
   var content = tmpl(maket, data);
 
-  $('body').append(content);
+  $('.wrapper').append(content);
 
 });
