7784bf13b71e4fab7138f6dfcb9d15c4d1d2c547
Dmytro Kotkin <kotkin.lg@gmail.com>
2016-05-15 18:23:06 +0300

    completed

--- js13-14/js/script.js
+++ js13-14/js/script.js
@@ -1,68 +1,188 @@
+'use strict;'
+
 $(function(){
 
+  var allQuestions = [
+    {
+        question: "Check the sentence where is used Present Continious tense:",
+        choices: ["She looked at a picture.", "We often take photos.",
+                  "They are riding a bike.", "I have just had a cup of tea." ]
+    },
+    {
+        question: "Check the sentence where is used Present Simple tense:",
+        choices: ["She looked at a picture.", "We often take photos.",
+                  "They are riding a bike.", "I have just had a cup of tea."]
+    },
+    {
+        question: "Check the sentence where is used Present Perfect tense:",
+        choices: ["She looked at a picture.", "We often take photos.",
+                  "They are riding a bike.", "I have just had a cup of tea."]
+    },
+    {
+        question: "Check the sentence where is used Past Simple tense:",
+        choices: ["She looked at a picture.", "We often take photos.",
+                  "They are riding a bike.", "I have just had a cup of tea."]
+    }
+  ];
+  var correctAnswer = {
+    one: 2,
+    two: 1,
+    three: 3,
+    four: 0
+  };
 
+  localStorage.setItem('questions', JSON.stringify(allQuestions));
+  var loadedQuestions = localStorage.getItem('questions');
+  loadedQuestions = JSON.parse(loadedQuestions);
 
-var objTest = {
+  localStorage.setItem('answers', JSON.stringify(correctAnswer));
+  var loadedAnswers = localStorage.getItem('answers');
+  loadedAnswers = JSON.parse(loadedAnswers);
 
-    docBody: document.body,
+  var submitBtn = $('#myBtn');
+  var currentQuestion = 0;
+  var tally = 0;
 
-    outputHeaderTest: function() {
-        var elementHeaderTest = document.createElement('h1');
-        elementHeaderTest.className = 'main-header';
-        elementHeaderTest.innerHTML = 'Тест по программированию';
-        elementHeaderTest.style.textAlign = 'center';
-        this.docBody.insertBefore(elementHeaderTest, this.docBody.firstChild);
+  var quizForm = $('#quiz');
+  var question;
+  var choices;
+  var radioButtons = [$('input[name="radioOption0"]'), $('input[name="radioOption1"]'), $('input[name="radioOption2"]'),
+                     $('input[name="radioOption3"]')];
+
+  submitBtn.attr('value', 'Проверить мои результаты');
+
+  var objTest = {
+
+    askQuestion: function () {
+
+        for (var j = 0; j < 4; j++) {
+          if (loadedQuestions) {
+            choices = loadedQuestions[currentQuestion].choices;
+            question = loadedQuestions[currentQuestion].question
+
+          } else {
+            choices = allQuestions[currentQuestion].choices;
+            question = allQuestions[currentQuestion].question;
+          }
+
+          quizForm.append(
+            $("<h2>" + (j + 1) + "." + question + "</h2>")
+          );
+
+            for (var i = 0; i < choices.length; i++) {
+              quizForm.append(
+                $("<label><input type='radio' name='radioOption" + j + "'value='" + choices[i] + "'/>" + choices[i] + "</label>"),
+                $('<br>')
+              );
+            }
+
+            currentQuestion++;
+        }
     },
 
-    outputQuestions: function() {
-        var elementForm = document.createElement('form');
-        this.docBody.insertBefore(elementForm, this.docBody.lastChild);
+    lookForChecked: function () {
+      if (loadedAnswers) {
+        answer = loadedAnswers;
 
-        for (var i = 1; i <= 3; i++) {
+      } else {
+        answer = correctAnswer;
+      }
 
-            var elementQuestionTitle = document.createElement('p');
-            elementQuestionTitle.className = 'form__question-title';
-            elementQuestionTitle.innerHTML = i+ '. Вопрос №' + i;
-            elementForm.appendChild(elementQuestionTitle);
+        if (radioButtons.length > 1) {
 
-            for (var j=1; j <= 3; j++) {
+          if ($("input:radio[name='radioOption0']")
+                .index($("input:radio[name='radioOption0']:checked")) == answer.one) {
+              tally++;
+          }
+          if ($("input:radio[name='radioOption1']")
+                .index($("input:radio[name='radioOption1']:checked")) == answer.two) {
+              tally++;
+          }
+          if ($("input:radio[name='radioOption2']")
+                .index($("input:radio[name='radioOption2']:checked")) == answer.three) {
+              tally++;
+          }
+          if ($("input:radio[name='radioOption3']")
+                .index($("input:radio[name='radioOption3']:checked")) == answer.four) {
+              tally++;
+          }
+        }
 
-                var elementDivForCheckbox = document.createElement('div');
-                elementDivForCheckbox.className = 'form__checkbox-position';
-                elementForm.appendChild(elementDivForCheckbox);
+            objTest.modal();
+            tally = 0;
+    },
 
-                var elementCheckbox = document.createElement('input');
-                elementCheckbox.type = 'checkbox';
-                elementCheckbox.className = 'form__checkbox';
-                elementCheckbox.id = 'id-checbox' + i + '_' + j;
-                elementDivForCheckbox.appendChild(elementCheckbox);
+    modal: function () {
+        var rates1 = $('input[name="radioOption0"]');
+        var rates2 = $('input[name="radioOption1"]');
+        var rates3 = $('input[name="radioOption2"]');
+        var rates4 = $('input[name="radioOption3"]');
 
-                var elementLabel = document.createElement('label');
-                elementLabel.className = 'form__label';
-                elementLabel.htmlFor = 'id-checbox' + i + '_' + j;
-                elementLabel.id = 'id-checbox' + i + '_' + j;
-                elementDivForCheckbox.appendChild(elementLabel);
+        var rate_value1 = 'undefined';
+        var rate_value2 = 'undefined';
+        var rate_value3 = 'undefined';
+        var rate_value4 = 'undefined';
 
-                var textCheckbox = document.createTextNode('Вариант ответа №' + j);
-               elementLabel.appendChild(textCheckbox);
+        for (var i = 0; i < rates1.length; i++) {
+            if (rates1[i].type == "radio" && rates1[i].checked) {
+                rate_value1 = rates1[i].value;
+            }
+        }
+        for (var i = 0; i < rates2.length; i++) {
+            if (rates2[i].type == "radio" && rates2[i].checked) {
+                rate_value2 = rates2[i].value;
+            }
+        }
+        for (var i = 0; i < rates3.length; i++) {
+            if (rates3[i].type == "radio" && rates3[i].checked) {
+                rate_value3 = rates3[i].value;
+            }
+        }
+        for (var i = 0; i < rates4.length; i++) {
+            if (rates4[i].type == "radio" && rates4[i].checked) {
+                rate_value4 = rates4[i].value;
             }
         }
 
-        var divForButton = document.createElement('div');
-        divForButton.className = 'form__button-position';
-        elementForm.appendChild(divForButton);
+        if (rate_value1 == 'undefined' || rate_value2 == 'undefined' || rate_value3 == 'undefined' || rate_value4 == 'undefined') {
+          alert("Не все ответы выбраны!");
+        } else {
+          $('.wrapper').append(
+            $('<div/>')
+              .addClass("modal-overlay")
+              .append('<div class = "modal-container"></div>')
+          );
+          $('.modal-container').append(
+            $('<h2>')
+              .text('Ваши результаты'),
+            $('</p>')
+              .addClass('results')
+              .text("Вы ответили правильно на " + tally + " вопрос(a) из " + loadedQuestions.length),
+              $('</p>')
+              .addClass('closeButton')
+              .text('Закрыть'),
+            $('</p>')
+              .addClass('clearStorage')
+              .text('Очистить внутреннее хранилище')
+          );
 
+          $('.clearStorage').one('click', function() {
+            localStorage.clear();
+          });
 
-        var formButton = document.createElement('input');
-        formButton.type = 'submit';
-        formButton.value = 'Проверить мои результаты';
-        formButton.className = 'form__button';
-        divForButton.appendChild(formButton);
-    }
-}
+          $(document).one("click", ".closeButton", function() {
+            $('.modal-overlay').remove();
+          });
 
-objTest.outputHeaderTest();
-objTest.outputQuestions();
+          $('input[type=radio]').prop('checked', false);
+        }
 
+      }
+
+  }
+
+  objTest.askQuestion();
+
+  submitBtn.on('click', objTest.lookForChecked);
 
 });
