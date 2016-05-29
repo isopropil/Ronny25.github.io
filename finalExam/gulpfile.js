fe192b823cb8cd57c4a1f106d544e404f9dff395
Dmytro Kotkin <kotkin.lg@gmail.com>
2016-05-29 18:36:25 +0300

    mobile view without ajax

---
+++ finalExam/gulpfile.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    sourcemaps = require('gulp-sourcemaps');
+
+var paths = {
+  style: 'production/sass/style.scss',
+  scripts: 'production/js/*.js',
+  images: 'production/img/*'
+};
+
+gulp.task('styles', function () {
+  return gulp.src(paths.style)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('build/css'));
+});
+
+gulp.task('scripts', function() {
+  // Minify and copy all JavaScript (except vendor scripts)
+  // with sourcemaps all the way down
+  return gulp.src(paths.scripts)
+    .pipe(sourcemaps.init())
+      .pipe(uglify())
+      .pipe(concat('script.min.js'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('build/js'));
+});
+
+// Copy all static images
+gulp.task('images', function() {
+  return gulp.src(paths.images)
+    // Pass in options to the task
+    .pipe(imagemin())
+    .pipe(gulp.dest('build/img'));
+});
+
+// Rerun the task when a file changes
+gulp.task('watch', function() {
+  gulp.watch(paths.style, ['styles']);
+  gulp.watch(paths.scripts, ['scripts']);
+  gulp.watch(paths.images, ['images']);
+});
+
+// The default task (called when you run `gulp` from cli)
+gulp.task('default', ['watch', 'styles', 'scripts', 'images']);
