8699aeab6abbca0c79d179af02849da3ba4085d2
Dmytro Kotkin <kotkin.lg@gmail.com>
2016-05-30 22:52:55 +0300

    tablet view, sliders, no AJAX

---
+++ finalExam/production/js/script.js
@@ -0,0 +1,26 @@
+$(function(){
+
+  // sliders
+  $('.slider1').sss({
+    slideShow : true, // Set to false to prevent SSS from automatically animating.
+    startOn : 0, // Slide to display first. Uses array notation (0 = first slide).
+    transition : 400, // Length (in milliseconds) of the fade transition.
+    speed : 3500, // Slideshow speed in milliseconds.
+    showNav : true // Set to false to hide navigation arrows.
+  });
+  $('.slider2').sss({
+    slideShow : true, // Set to false to prevent SSS from automatically animating.
+    startOn : 1, // Slide to display first. Uses array notation (0 = first slide).
+    transition : 400, // Length (in milliseconds) of the fade transition.
+    speed : 3500, // Slideshow speed in milliseconds.
+    showNav : true // Set to false to hide navigation arrows.
+  });
+  $('.slider3').sss({
+    slideShow : true, // Set to false to prevent SSS from automatically animating.
+    startOn : 2, // Slide to display first. Uses array notation (0 = first slide).
+    transition : 400, // Length (in milliseconds) of the fade transition.
+    speed : 3500, // Slideshow speed in milliseconds.
+    showNav : true // Set to false to hide navigation arrows.
+  });
+
+});
